e148186286a3cff129767f5132d916bbce0b2f45
Aleksei Beljajev <alekbeljak@gmail.com>
2019-10-19 22:25:45 +0300

    Added layer of abstraction to progress bar class, added progress icon(optional), add shader graph for 'road' bar

---
+++ Assets/Scripts/Editor/UIHelpersMenuItems.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class UIHelpersMenuItems : Editor
+{
+    public const string CreationPath = "GameObject/UI/UIHelpers/";
+    
+    [MenuItem(CreationPath + "Linear Progress Bar")]
+    public static void AddLinearProgressBar()
+    {
+        GameObject barObj = Instantiate(
+            Resources.Load<GameObject>("UIHelpers/Progress Bar Linear"),
+            Selection.activeGameObject.transform, false);
+        barObj.name = "Progress Bar Linear";
+    }
+        
+    [MenuItem(CreationPath + "Radial Progress Bar")]
+    public static void AddRadialProgressBar()
+    {
+        GameObject barObj = Instantiate(
+            Resources.Load<GameObject>("UIHelpers/Progress Bar Radial"),
+            Selection.activeGameObject.transform, false);
+        barObj.name = "Progress Bar Radial";
+    }
+}
--- Assets/Scripts/UIHelpers/ProgressBar.cs
+++ Assets/Scripts/UIHelpers/ProgressBar.cs
@@ -1,57 +1,76 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace UIHelpers
 {
     [ExecuteInEditMode]
     public class ProgressBar : MonoBehaviour
     {
-        [SerializeField] private Image _maskImage;
-        [SerializeField] private Image _barImage;
+        [SerializeField] protected Image _maskImage;
+        [SerializeField] protected Image _barImage;
         
-        [SerializeField] private float _minimumValue;
-        [SerializeField] private float _maximumValue;
-        [SerializeField] private float _currentValue;
-        [SerializeField] private Gradient _gradient;
+        [SerializeField] protected float _minimumValue;
+        [SerializeField] protected float _maximumValue;
+        [SerializeField] protected int _stepValue = 1;
+        [SerializeField] protected float _currentValue;
+        
+        [SerializeField] protected Image _progressIcon;
+        [SerializeField] protected Gradient _gradient;
 
-        private void SetFillAmount(float fillAmount)
+        private Vector3 _barLeftSide;
+        private Vector3 _barRightSide;
+        
+        private bool _isMaskImage, _isBarImage, _isProgressIconImage;
+
+        private void OnAwake()
         {
-            _currentValue = fillAmount;
-            fillAmount = Mathf.InverseLerp(_minimumValue, _maximumValue, fillAmount);
-            _maskImage.fillAmount = fillAmount;
-            _barImage.color = _gradient.Evaluate(fillAmount);
+            CheckReferencesForNull();
         }
 
-        private void OnValidate()
+        private void CheckReferencesForNull()
         {
+            _isMaskImage = _maskImage != null;
+            _isBarImage = _barImage != null;
+            _isProgressIconImage = _progressIcon != null;
+        }
+
+        protected virtual void SetFillAmount(float fillAmount)
+        {
+            if (_isMaskImage == false || _isBarImage == false)
+                return;
+            
+            _currentValue = fillAmount;
+            float steppedValue = (int) fillAmount / _stepValue * _stepValue;
+            
+            fillAmount = Mathf.InverseLerp(_minimumValue, _maximumValue, steppedValue);
+            
+            _maskImage.fillAmount = fillAmount;
+            _barImage.color = _gradient.Evaluate(fillAmount);
+
+            if (_isProgressIconImage == false)
+                return;
+            _progressIcon.rectTransform.position = Vector3.Lerp(_barLeftSide, _barRightSide, fillAmount);
+        }
+
+        private void SetBarSides()
+        {
+            Vector3[] corners = new Vector3[4];
+            _maskImage.rectTransform.GetWorldCorners(corners);
+            _barLeftSide = (corners[0] + corners[1]) / 2;
+            _barRightSide = (corners[2] + corners[3]) / 2;
+        }
+
+        protected virtual void OnValidate()
+        {
+            CheckReferencesForNull();
+            SetBarSides();
+            if (_stepValue == 0)
+                _stepValue = 1;
+
             _currentValue = _currentValue >= _maximumValue ? _maximumValue : _currentValue;
             _currentValue = _currentValue <= _minimumValue ? _minimumValue : _currentValue;
 
             SetFillAmount(_currentValue);
         }
-        
-            
-#if UNITY_EDITOR
-        [MenuItem("GameObject/UI/Linear Progress Bar")]
-        public static void AddLinearProgressBar()
-        {
-            GameObject barObj = Instantiate(
-                Resources.Load<GameObject>("UIHelpers/Progress Bar Linear"),
-                Selection.activeGameObject.transform, false);
-            barObj.name = "Progress Bar Linear";
-        }
-        
-        [MenuItem("GameObject/UI/Radial Progress Bar")]
-        public static void AddRadialProgressBar()
-        {
-            GameObject barObj = Instantiate(
-                Resources.Load<GameObject>("UIHelpers/Progress Bar Radial"),
-                Selection.activeGameObject.transform, false);
-            barObj.name = "Progress Bar Radial";
-        }
-#endif
     }
 }
