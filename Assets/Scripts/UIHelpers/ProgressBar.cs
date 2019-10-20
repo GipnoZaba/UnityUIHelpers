e160a7c3f1d14dc8d0dddcf706b82fca5a71bda7
Aleksei Beljajev <alekbeljak@gmail.com>
2019-10-20 14:20:37 +0300

    Text values

--- Assets/Scripts/UIHelpers/ProgressBar.cs
+++ Assets/Scripts/UIHelpers/ProgressBar.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace UIHelpers
@@ -6,23 +8,27 @@ namespace UIHelpers
     [ExecuteInEditMode]
     public class ProgressBar : MonoBehaviour
     {
-        [SerializeField] protected Image _maskImage;
-        [SerializeField] protected Image _barImage;
-        
-        [SerializeField] protected float _minimumValue;
-        [SerializeField] protected float _maximumValue;
-        [SerializeField] protected int _stepValue = 1;
-        [SerializeField] protected float _currentValue;
-        
-        [SerializeField] protected Image _progressIcon;
-        [SerializeField] protected Gradient _gradient;
+        [SerializeField] private Image _maskImage;
+        [SerializeField] private Image _barImage;
+        [SerializeField] private Image _progressIcon;
+        [Space(20)] 
+        [SerializeField] private TextMeshProUGUI _textMeshNormalised;
+        [SerializeField] private TextMeshProUGUI _textMeshValue;
+        [Space(20)]
+        [SerializeField] private float _minimumValue;
+        [SerializeField] private float _maximumValue;
+        [SerializeField] private int _stepValue = 1;
+        [SerializeField] private float _currentValue;
+        [Space(20)]
+        [SerializeField] private Gradient _barGradient;
+        [SerializeField] private Gradient _progressPointGradient;
 
-        private Vector3 _barLeftSide;
-        private Vector3 _barRightSide;
+        private Vector3 _barProgressStart;
+        private Vector3 _barProgressEnd;
         
-        private bool _isMaskImage, _isBarImage, _isProgressIconImage;
+        private bool _isMaskImage, _isBarImage, _isProgressIconImage, _isTextMeshNormalised, _isTextMeshValue;
 
-        private void OnAwake()
+        private void Awake()
         {
             CheckReferencesForNull();
         }
@@ -32,38 +38,68 @@ namespace UIHelpers
             _isMaskImage = _maskImage != null;
             _isBarImage = _barImage != null;
             _isProgressIconImage = _progressIcon != null;
+            _isTextMeshNormalised = _textMeshNormalised != null;
+            _isTextMeshValue = _textMeshValue != null;
         }
 
-        protected virtual void SetFillAmount(float fillAmount)
+        public void SetFillAmount(float fillAmount)
         {
             if (_isMaskImage == false || _isBarImage == false)
                 return;
             
-            _currentValue = fillAmount;
-            float steppedValue = (int) fillAmount / _stepValue * _stepValue;
-            
-            fillAmount = Mathf.InverseLerp(_minimumValue, _maximumValue, steppedValue);
-            
-            _maskImage.fillAmount = fillAmount;
-            _barImage.color = _gradient.Evaluate(fillAmount);
+            fillAmount = CalculateFillFromValue(fillAmount);
 
-            if (_isProgressIconImage == false)
-                return;
-            _progressIcon.rectTransform.position = Vector3.Lerp(_barLeftSide, _barRightSide, fillAmount);
+            _maskImage.fillAmount = fillAmount;
+            _barImage.color = _barGradient.Evaluate(fillAmount);
+            
+            AdjustProgressPoint(fillAmount);
+            UpdateText(fillAmount);
         }
 
-        private void SetBarSides()
+        private void AdjustProgressPoint(float fillAmount)
+        {
+            if (_isProgressIconImage == false)
+                return;
+
+            _progressIcon.color = _progressPointGradient.Evaluate(fillAmount);
+            _progressIcon.rectTransform.position = Vector3.Lerp(_barProgressStart, _barProgressEnd, fillAmount);
+        }
+
+        private void UpdateText(float fillAmount)
+        {
+            if (_isTextMeshNormalised)
+            {
+                _textMeshNormalised.SetText(Math.Round(fillAmount, 2).ToString());
+            }
+
+            if (_isTextMeshValue)
+            {
+                _textMeshValue.SetText(Math.Round(_currentValue).ToString());
+            }
+        }
+
+        private float CalculateFillFromValue(float fillAmount)
+        {
+            _currentValue = fillAmount;
+            float steppedValue = (int) fillAmount / _stepValue * _stepValue;
+
+            fillAmount = Mathf.InverseLerp(_minimumValue, _maximumValue, steppedValue);
+            return fillAmount;
+        }
+
+        private void CalculateBarEnds()
         {
             Vector3[] corners = new Vector3[4];
             _maskImage.rectTransform.GetWorldCorners(corners);
-            _barLeftSide = (corners[0] + corners[1]) / 2;
-            _barRightSide = (corners[2] + corners[3]) / 2;
+            _barProgressStart = (corners[0] + corners[1]) / 2;
+            _barProgressEnd = (corners[2] + corners[3]) / 2;
         }
 
-        protected virtual void OnValidate()
+        private void OnValidate()
         {
             CheckReferencesForNull();
-            SetBarSides();
+            CalculateBarEnds();
+            
             if (_stepValue == 0)
                 _stepValue = 1;
 
